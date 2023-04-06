3f4580a6ba1ceca9b231cf1114bc59d39f8193c7
PhanTien234 <minhtienp328@gmail.com>
2023-04-07 00:21:42 +0700

    make form to binding data

--- Pages/ContactRequest.cshtm.cs
+++ Pages/ContactRequest.cshtm.cs
@@ -10,8 +10,11 @@ public class ContactRequestModel : PageModel{
     [Range(10,100, ErrorMessage = "Nhap sai")]
     public int UserId {get;set;}
     [BindProperty]
+    [DisplayName("Email cua ban")]
+    [EmailAddress(ErrorMessage ="Email sai dinh dang")]
     public string Email {get;set;}
     [BindProperty]
+    [DisplayName("Ten nguoi dung")]
     public string UserName {get; set;}
 
     private readonly ILogger<ContactRequestModel> _logger;
