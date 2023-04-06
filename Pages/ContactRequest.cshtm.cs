2f60a333b476b7655b9bd89b97f3194ee10c9076
PhanTien234 <minhtienp328@gmail.com>
2023-04-07 00:09:16 +0700

    use attribute [Range] to limit the value of UserId and display the error if you type over the range

--- Pages/ContactRequest.cshtm.cs
+++ Pages/ContactRequest.cshtm.cs
@@ -1,11 +1,13 @@
 
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 public class ContactRequestModel : PageModel{
     [BindProperty]
     [DisplayName("Id cua ban")]
+    [Range(10,100, ErrorMessage = "Nhap sai")]
     public int UserId {get;set;}
     [BindProperty]
     public string Email {get;set;}
